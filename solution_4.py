50d784058868bd7166e09a175634f0961ab4d624
Alleg <opankov@inbox.ru>
2024-03-24 22:19:51 +0300

    new_fixed

--- solution_4.py
+++ solution_4.py
@@ -1,19 +1,17 @@
 # Задание 4: Валидация входных данных для расчёта сроков
 
-import sys
-
-def validate_input(func):
-    def wrapper(project_name, num_tasks):
-        if len(sys.argv) != 1:
+def check_arguments(func):
+    def wrapper(*args):
+        if len(args) != 2:
             return 'Ошибка: Неверное количество аргументов!'
-        if not isinstance(project_name, str):
+        if not isinstance(args[0], str):
             return 'Ошибка: Первый аргумент не строка!'
-        if not isinstance(num_tasks, int):
+        if not isinstance(args[1], int):
             return 'Ошибка: Второй аргумент не число!'
-        return func(project_name, num_tasks)
+        return func(*args)
     return wrapper
 
-@validate_input
+@check_arguments
 def estimate_time(project_name, num_tasks):
     return 'Estimated time calculated successfully.'
 
